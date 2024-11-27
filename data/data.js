19b75012443d8f6dc92e83070dc6d3378d3c35a9
CamilleTD <55928087+CamilleTD@users.noreply.github.com>
2024-11-27 17:04:49 -0500

    Update data.js

--- data/data.js
+++ data/data.js
@@ -1,33 +1,31 @@
-const dataItems = [
-    {
-      title: 'Cows',
-      desc: 'Cows are large domesticated animals commonly raised for their milk, meat, and leather. They are herbivores and often found on farms.',
-      img: '../assets/cow.jpg'
-    },
-    {
-      title: 'Chickens',
-      desc: 'Chickens are domesticated fowl raised for their eggs and meat. They are omnivores and are one of the most common farm animals worldwide.',
-      img: '../assets/chicken.jpg'
-    },
-    {
-      title: 'Pigs',
-      desc: 'Pigs are intelligent and social animals raised primarily for their meat, known as pork. They are omnivores and thrive in various farm environments.',
-      img: '../assets/pig.jpg'
-    },
-    {
-      title: 'Sheep',
-      desc: 'Sheep are domesticated herbivores raised for their wool, milk, and meat. They are often found in pastures and are important in farming cultures worldwide.',
-      img: '../assets/sheep.jpg'
-    },
-    {
-      title: 'Goats',
-      desc: 'Goats are hardy and curious animals raised for their milk, meat, and fiber. They are herbivores and can adapt to various climates and terrains.',
-      img: '../assets/goat.jpg'
-    },
-    {
-      title: 'Ducks',
-      desc: 'Ducks are waterfowl often raised for their eggs, meat, and feathers. They are omnivores and are commonly found on farms, especially near water.',
-      img: '../assets/duck.jpg'
-    }
-  ];
-  
+const dataItems = [{
+  title: 'Cows',
+  desc: 'Cows are large domesticated animals commonly raised for their milk, meat, and leather. They are herbivores and often found on farms.',
+  url: 'https://i.pinimg.com/736x/3f/35/09/3f35095e5e2c651496235dfd77aba747.jpg'
+},
+{
+  title: 'Chickens',
+  desc: 'Chickens are domesticated fowl raised for their eggs and meat. They are omnivores and are one of the most common farm animals worldwide.',
+  url: 'https://i.pinimg.com/736x/52/f5/ab/52f5abffe6c66e5c85fa2b53dee79cc9.jpg'
+},
+{
+  title: 'Pigs',
+  desc: 'Pigs are intelligent and social animals raised primarily for their meat, known as pork. They are omnivores and thrive in various farm environments.',
+  url: 'https://i.pinimg.com/736x/07/d2/51/07d25183512a688a4ab2a5dcc9098d72.jpg'
+},
+{
+  title: 'Sheep',
+  desc: 'Sheep are domesticated herbivores raised for their wool, milk, and meat. They are often found in pastures and are important in farming cultures worldwide.',
+  url: 'https://i.pinimg.com/736x/f7/11/51/f711518b9f831509cd72f9cc937589b4.jpg'
+},
+{
+  title: 'Goats',
+  desc: 'Goats are hardy and curious animals raised for their milk, meat, and fiber. They are herbivores and can adapt to various climates and terrains.',
+  url: 'https://i.pinimg.com/736x/82/f2/51/82f25113707f213e9724950b5da71505.jpg'
+},
+{
+  title: 'Ducks',
+  desc: 'Ducks are waterfowl often raised for their eggs, meat, and feathers. They are omnivores and are commonly found on farms, especially near water.',
+  url: 'https://i.pinimg.com/736x/a3/bf/ad/a3bfad095a188864cbb0dadbb0dbc638.jpg'
+}
+];
